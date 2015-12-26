4b7d3a0f59c460125ba176e2d2f3984525f80dce
jeffslutzky <jeffslutzky@gmail.com>
2015-12-26 00:19:46 -0500

    nearing completion of chart

--- app/assets/javascripts/presidents.js
+++ app/assets/javascripts/presidents.js
@@ -31,22 +31,28 @@ function makeChart(presidents) {
 function d3Chart(presidents){
   var data = presidents.days;
 
-  var width = 1000,
-    height = 650;
+  var margin = {top: 20, right: 20, bottom: 180, left: 40},
+      width = 1100 - margin.left - margin.right,
+      height = 650 - margin.top - margin.bottom;
+
+  var x = d3.scale.ordinal()
+      .domain(presidents.names)
+      .rangeRoundBands([0, width], .1);
 
   var y = d3.scale.linear()
     .domain([0, d3.max(data)])
     .range([height, 0]);
 
-  var margin = {top: 20, right: 30, bottom: 30, left: 40},
-      width = 1000 - margin.left - margin.right,
-      height = 650 - margin.top - margin.bottom;
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
 
   var chart = d3.select("#myChart")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
     .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 
   var barWidth = width / data.length;
 
@@ -56,15 +62,24 @@ function d3Chart(presidents){
       .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
 
   bar.append("rect")
-      .attr("y", function(d) { return y(d) - 20; })
-      .attr("height", function(d) { return height - y(d) + 50; })
+      .attr("y", function(d) { return y(d); })
+      .attr("height", function(d) { return height - y(d) + 1; })
       .attr("width", barWidth - 1);
+  //
+  // bar.append("text")
+  //     .attr("x", barWidth / 2 + 11)
+  //     .attr("y", function(d) { return y(d) -17; })
+  //     .attr("dy", ".75em")
+  //     .text(function(d) { return d; });
 
-  bar.append("text")
-      .attr("x", barWidth / 2 + 11)
-      .attr("y", function(d) { return y(d) -17; })
-      .attr("dy", ".75em")
-      .text(function(d) { return d; });
-
+  chart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.2em")
+        .attr("dy", ".55em")
+        .attr("transform", "rotate(-50)" );
 
 };
