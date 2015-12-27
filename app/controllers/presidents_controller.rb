7a881d26c02abf505499337ef63f9c88fe1cb7d0
jeffslutzky <jeffslutzky@gmail.com>
2015-12-26 23:50:43 -0500

    add line breaks to controller

--- app/controllers/presidents_controller.rb
+++ app/controllers/presidents_controller.rb
@@ -5,7 +5,10 @@ class PresidentsController < ApplicationController
     sorted_hash = RetirementsViewObject.new(President.all).get_data_sorted
     respond_to do |format|
       format.html { }
-      format.json { render json: {chronological: {names: hash.keys, days: hash.values}, descending: {names: sorted_hash.keys, days: sorted_hash.values} } }
+      format.json { render json: {
+        chronological: {names: hash.keys, days: hash.values},
+        descending: {names: sorted_hash.keys, days: sorted_hash.values}
+      } }
     end
   end
 
