797009a100eaa88b08fbe7ee27af0c9712c58d0e
jeffslutzky <jeffslutzky@gmail.com>
2015-12-29 01:53:59 -0500

    fix heroku

--- public/assets/application-b76c337f76a40ce68e933632a87a3942acf527fd4f272a200d4ea703f85cc42f.js
+++ public/assets/application-b76c337f76a40ce68e933632a87a3942acf527fd4f272a200d4ea703f85cc42f.js
@@ -11640,29 +11640,29 @@ return jQuery;
   };
 
 }).call(this);
-$(function() {
-  $.getJSON('', function(data){
-  makeChart(data);
-  });
-});
-
-function makeChart(presidents) {
-  var ctx = $("#myChart").get(0).getContext("2d");
-  var data = {
-    labels: presidents.names,
-    datasets: [
-      {
-        label: 'Retirement Lengths of U.S. Presidents',
-        fillColor: 'red',
-        strokeColor: 'rgba(220,220,220,0.8)',
-        highlightFill: 'blue',
-        highlightStroke: 'rgba(220,220,220,1)',
-        data: presidents.days
-      }
-    ]
-  };
-  var myBarChart = new Chart(ctx).Bar(data);
-};
+// $(function() {
+//   $.getJSON('', function(data){
+//   makeChart(data);
+//   });
+// });
+//
+// function makeChart(presidents) {
+//   var ctx = $("#myChart").get(0).getContext("2d");
+//   var data = {
+//     labels: presidents.names,
+//     datasets: [
+//       {
+//         label: 'Retirement Lengths of U.S. Presidents',
+//         fillColor: 'red',
+//         strokeColor: 'rgba(220,220,220,0.8)',
+//         highlightFill: 'blue',
+//         highlightStroke: 'rgba(220,220,220,1)',
+//         data: presidents.days
+//       }
+//     ]
+//   };
+//   var myBarChart = new Chart(ctx).Bar(data);
+// };
 /*!
  * Chart.js
  * http://chartjs.org/
