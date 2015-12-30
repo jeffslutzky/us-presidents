d1a9c24f4219975ed72752ec83d66d7d331325d5
jeffslutzky <jeffslutzky@gmail.com>
2015-12-30 11:06:31 -0500

    heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += ['presidents.js', 'presidents.scss']
+Rails.application.config.assets.precompile += ['presidents.js', 'presidents.scss', 'index.js']
