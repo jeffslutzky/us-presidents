b958436cd0d848e7105b1c2f3eec69e17b5f67db
jeffslutzky <jeffslutzky@gmail.com>
2015-12-30 11:58:32 -0500

    heroku

--- app/assets/javascripts/presidents.js
+++ app/assets/javascripts/presidents.js
@@ -29,12 +29,12 @@ function retirements(data){
         .orient("left")
         .ticks(10);
 
-    var tip = d3.tip()
-      .attr('class', 'd3-tip')
-      .offset([-10, 0])
-      .html(function(d) {
-        return d[1] + ": " + d[2];
-      })
+    // var tip = d3.tip()
+    //   .attr('class', 'd3-tip')
+    //   .offset([-10, 0])
+    //   .html(function(d) {
+    //     return d[1] + ": " + d[2];
+    //   })
 
     var svg = d3.select(".container")
       .append("svg")
@@ -43,7 +43,7 @@ function retirements(data){
       .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    svg.call(tip);
+    // svg.call(tip);
 
     svg.append("g")
         .attr("class", "x axis")
@@ -73,8 +73,8 @@ function retirements(data){
         .attr("y", function(d) { return yScale(d[2]); })
         .attr("height", function(d) { return height - yScale(d[2]) + 1; })
         .attr("width", xScale.rangeBand()-1)
-        .on('mouseover', tip.show)
-        .on('mouseout', tip.hide);
+        // .on('mouseover', tip.show)
+        // .on('mouseout', tip.hide);
 
     var sortOrder = false;
 
--- public/assets/index-55d80fdb358d5dcb0a019e203a85cc371c4f7410f53f78741766053d3fa10855.js
+++ public/assets/index-55d80fdb358d5dcb0a019e203a85cc371c4f7410f53f78741766053d3fa10855.js
@@ -4,19 +4,19 @@
 // Tooltips for d3.js SVG visualizations
 
 (function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module with d3 as a dependency.
-    define(['d3'], factory)
-  } else if (typeof module === 'object' && module.exports) {
-    // CommonJS
-    module.exports = function(d3) {
-      d3.tip = factory(d3)
-      return d3.tip
-    }
-  } else {
-    // Browser global.
-    root.d3.tip = factory(root.d3)
-  }
+  // if (typeof define === 'function' && define.amd) {
+  //   // AMD. Register as an anonymous module with d3 as a dependency.
+  //   define(['d3'], factory)
+  // } else if (typeof module === 'object' && module.exports) {
+  //   // CommonJS
+  //   module.exports = function(d3) {
+  //     d3.tip = factory(d3)
+  //     return d3.tip
+  //   }
+  // } else {
+  //   // Browser global.
+  //   root.d3.tip = factory(root.d3)
+  // }
 }(this, function (d3) {
 
   // Public - contructs a new tooltip
