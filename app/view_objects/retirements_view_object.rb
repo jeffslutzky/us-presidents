7d890c45fccaf9932ed563befcee54f054305d36
jeffslutzky <jeffslutzky@gmail.com>
2016-01-02 17:00:55 -0500

    fix order of array

--- app/view_objects/retirements_view_object.rb
+++ app/view_objects/retirements_view_object.rb
@@ -7,7 +7,7 @@ class RetirementsViewObject
 
   def get_data
     data_array = @presidents.each_with_object([]) do |president, array|
-      array << [president.id, president.name_firstlast, president.alive?, president.retirement_in_days, president.lifespan, president.age_at_inauguration]
+      array << [president.id, president.name_firstlast, president.alive?, president.age_at_inauguration, president.retirement_in_days, president.lifespan]
     end
     data_array.reject{|prez| prez[0] == 22}
   end
